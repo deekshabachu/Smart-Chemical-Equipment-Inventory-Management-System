ba7d4761bf70be1d729e890e33949f96fe751027
Deeksha <bachu.deeksha@gmail.com>
2026-06-26 19:04:35 +0530

    Implement authentication, role-based dashboards, and chemical inventory frontend

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", home, name="home"),
     path("hod/", hod_dashboard, name="hod_dashboard"),
     path("labtech/", labtech_dashboard, name="labtech_dashboard"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("chemicals/", chemicals, name="chemicals"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
 from .models import Chemical, Equipment, Alert, Lab
+from .models import UserProfile
 
 def home(request):
     return render(request, "inventory/home.html")
@@ -27,3 +31,79 @@ def labtech_dashboard(request):
     }
 
     return render(request, "inventory/dashboard_labtech.html", context)
+
+def login_view(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+
+            login(request, user)
+
+            if user.groups.filter(name="HOD").exists():
+
+                return redirect("hod_dashboard")
+
+            elif user.groups.filter(name="Lab Technician").exists():
+
+                return redirect("labtech_dashboard")
+
+            else:
+
+                return redirect("home")
+
+        else:
+
+            messages.error(
+                request,
+                "Invalid username or password."
+            )
+
+    return render(
+        request,
+        "inventory/login.html"
+    )
+
+
+def logout_view(request):
+
+    logout(request)
+
+    return redirect("home")
+
+def chemicals(request):
+
+    if request.user.groups.filter(name="HOD").exists():
+        chemicals = Chemical.objects.all()
+    else:
+        user_lab = request.user.userprofile.lab
+        chemicals = Chemical.objects.filter(lab=user_lab)
+
+    # Search
+    search = request.GET.get("search")
+
+    if search:
+        chemicals = chemicals.filter(name__icontains=search)
+
+    # Hazard Filter
+    hazard = request.GET.get("hazard")
+
+    if hazard:
+        chemicals = chemicals.filter(hazard_level=hazard)
+
+    context = {
+        "chemicals": chemicals,
+        "search": search,
+        "hazard": hazard,
+    }
+
+    return render(request, "inventory/chemicals.html", context)
